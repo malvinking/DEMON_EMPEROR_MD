e1780d062fc6a40e22799e8dcad794f21f31ee70
Malvin King <163745886+malvinking@users.noreply.github.com>
2024-06-25 13:58:36 +0200

    Update repos.js

--- malvin/repos.js
+++ malvin/repos.js
@@ -32,7 +32,7 @@ function _0x5403() {
         '35660asHjob',
         'i.github.c',
         'defineProp',
-        '𝐁𝐄𝐋𝐓𝐀𝐇\x20𝐓𝐄𝐂',
+        'DEMON\x20𝐓𝐄𝐂',
         'https://ap',
         'SIT\x20FOR\x20TU',
         '\x20REPOSITO',
@@ -77,7 +77,7 @@ function _0x5403() {
         'om/repos/B',
         'dee6cc7597',
         'en-GB',
-        'm/@Beltaht',
+        'm/@malvinking',
         '1375650kPEVMp',
         'https://te',
         '_count',
@@ -111,7 +111,7 @@ function _0x5403() {
         'tah\x20tech◆',
         'updated_at',
         '_______\x0a\x20◆',
-        'AM\x20Beltah-',
+        'AM\x20DEMON-',
         '\x20😊Hello,I\x20'
     ];
     _0x5403 = function () {
