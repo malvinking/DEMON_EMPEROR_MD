4f92ff0c46aede827daed507df38681bbe5cfde8
Malvin King <163745886+malvinking@users.noreply.github.com>
2024-06-25 15:47:58 +0200

    Add files via upload

---
+++ beltah/updates.js
@@ -0,0 +1,116 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const { zokou } = require("../framework/zokou");
+zokou({ nomCom: "wagroup", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = 'Hello 👋\n\nClick The link below to Join ᴅᴇᴍᴏɴ_ᴇᴍᴩᴇʀᴏʀ_ᴍᴅ WhatsApp Testing Group\n\n';
+    let d = 'https://chat.whatsapp.com/CibIQyQI3Em1HbUD0aVXez';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/a568780a23f9a324cdf4f.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = 'Hello 👋\n\nClick The link below to Join ᴅᴇᴍᴏɴ_ᴇᴍᴩᴇʀᴏʀ_ᴍᴅ WhatsApp Channel\n\n';
+    let d = 'https://whatsapp.com/channel/0029Vac8SosLY6d7CAFndv3Z';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/93ab99085bd915aaf22a4.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "developer", reaction: "🌟", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *HERE ARE MY OWNER INFO* \n\n✇ *NAME* : Malvin K Banda\n✇ *AGE* : 19 years\n✇ *SEX* : MALE\n✇ *RELIGION* : Christian\n✇ *NATIONALITY* :Zimbabwe 🇿🇼\n✇ *FAV TEAM* :???\n✇ *REAL LIFE* :I am a hacker, I learned little about coding.\n✇ *HOBBY* : Scrolling on websites, Reading manga or animes\n✇ *CONTACT ᴍᴀʟᴠɪɴ ᴋɪɴɢ * :👇 👇 \n';
+    let d = '𓄂 https://wa.me/263780166288\n𓄂 https://wa.me/263780934873\n\n *THANK YOU FOR LOVING ᴅᴇᴍᴏɴ_ᴇᴍᴩᴇʀᴏʀ_ᴍᴅ*. ';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/a568780a23f9a324cdf4f.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "test", reaction: "✅", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *☹︎ ᴅᴇᴍᴏɴ_ᴇᴍᴩᴇʀᴏʀ_ᴍᴅ BOT IS CURRENTLY ACTIVE ☹︎*\n';
+    let d = ' ❄︎ *_MADE ON EARTH BY ᴍᴀʟᴠɪɴ ᴋɪɴɢ_* ❄︎';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/a568780a23f9a324cdf4f.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "bot", reaction: "🤖", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *ᴅᴇᴍᴏɴ_ᴇᴍᴩᴇʀᴏʀ_ᴍᴅ MAIN FEATURES* \n\n 1.Auto view status✅\n 2.Always online✅\n 3.A1 chat gpt enabled \n 4.Download songs/images✅\n 5.Open view once photos✅\n 6.Antidelete messages✅\n 7.Auto block contacts✅\n 8.Auto bluetick✅\n\n';
+    let d = ' *THANK YOU FOR LOVING ᴅᴇᴍᴏɴ_ᴇᴍᴩᴇʀᴏʀ_ᴍᴅ*';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/93ab99085bd915aaf22a4.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "g*y", reaction: "🏳️‍🌈", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *F*CK MEHN,,, THIS GUY IS G*Y🏳️‍🌈🏳️‍🌈🏳️‍🌈🏳️‍🌈*\n';
+    let d = 'WHY WOULD YOU REQUEST THAT COMMAND IF NOT';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/5d4c27c77b240483e7b05.png';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "check", reaction: "🗿", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *ᴅᴇᴍᴏɴ_ᴇᴍᴩᴇʀᴏʀ_ᴍᴅ UPDATES*\n\n';
+    let d = 'Checking for updates...⏳\n\nUpdating new version...⏳\n\ndone....✅\n\n *ᴅᴇᴍᴏɴ_ᴇᴍᴩᴇʀᴏʀ_ᴍᴅ NEW VERSION SUCCESSFULLY INSTALLED...* ';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/a568780a23f9a324cdf4f.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "version", reaction: "⏳", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = ' *YOU ARE CURRENTLY USING ᴅᴇᴍᴏɴ_ᴇᴍᴩᴇʀᴏʀ_ᴍᴅ V3.0.0*\n\n';
+    let d = '✅latest version installed...';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/93ab99085bd915aaf22a4.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+
+zokou({ nomCom: "bugmenu", reaction: "🐛", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = '┌─❒ᴅᴇᴍᴏɴ_ᴇᴍᴩᴇʀᴏʀ_ᴍᴅ 𝗕𝗨𝗚 𝗠𝗘𝗡𝗨⁠⁠⁠⁠❒⁠⁠⁠\n│╭─────────────···\n┴│▸\n⬡│▸ ʙᴜɢ\n⬡│▸ ʙᴜɢᴠ\n⬡│▸ ʙᴜɢʀᴇᴀᴄᴛ\n⬡│▸ ᴀᴜᴅɪᴏʙᴜɢ\n⬡│▸ ɪᴏꜱʙᴜɢ\n⬡│▸ ɪᴏꜱʙᴜɢᴠ\n┬│▸\n│╰────────────···▸▸\n';
+    let d = '└───────────────···▸';
+    let varmess = z + d;
+    var img = 'https://telegra.ph/file/a568780a23f9a324cdf4f.jpg';
+    await zk.sendMessage(dest, { image: { url: img }, caption: varmess });
+    //console.log("montest")
+});
+console.log("mon test");
+/*module.exports.commande = () => {
+  var nomCom = ["test","t"]
+  var reaction="☺️"
+  return { nomCom, execute,reaction }
+};
+
+async function  execute  (origineMessage,zok) {
+  console.log("Commande saisie !!!s")
+   let z ='Salut je m\'appelle *Zokou* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Djalega++*'
+      let varmess=z+d
+      var img='https://wallpapercave.com/uwp/uwp3842939.jpeg'
+await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
+}  */ 
