86a4a64453663e28ca4f445e23d1fc2dbc2a8b0b
Malvin King <163745886+malvinking@users.noreply.github.com>
2024-06-25 14:21:34 +0200

    Update set.js

--- set.js
+++ set.js
@@ -9,7 +9,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || 'zokk',
     PREFIXE: process.env.PREFIX || "+",
-    OWNER_NAME: process.env.OWNER_NAME || "Beltah Tech",
+    OWNER_NAME: process.env.OWNER_NAME || "ᴍᴀʟᴠɪɴ ᴋɪɴɢ ",
     NUMERO_OWNER : process.env.NUMERO_OWNER || "ᴍᴀʟᴠɪɴ ᴋɪɴɢ ",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
